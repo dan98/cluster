65f5b38f4d870841604b7d3e487106eff703cabe
D.Grosu@student.rug.nl <s3135527@peregrine.hpc.rug.nl>
2016-09-20 10:08:43 +0200

    setup2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hello there. Those days are gone :)";
+	cout<<"Hello there. Those days are gone :) \n";
+	cout<<"Hello from the cluster :)";
+	cout<<"\n";
 	return 0;
 }
